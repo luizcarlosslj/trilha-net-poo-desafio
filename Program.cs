ccecf97687b00f46a81b9cd03bdf7a4c72f90f79
Luiz Carlos <luizcarlosslj@gmail.com>
2024-11-20 00:39:32 -0300

    Desafio celular concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Criando Smatphone Nokia");
+Nokia n = new Nokia("231", "2200b", "asd*", 32);
+n.Ligar();
+n.ReceberLigacao();
+n.InstalarAplicativo("DioApp");
+
+Console.WriteLine("---------------------------");
+
+Console.WriteLine("Criando Smatphone Iphone");
+Iphone p = new Iphone("456", "I14x", "$#%", 512);
+p.Ligar();
+p.ReceberLigacao();
+p.InstalarAplicativo("Icloud");
+
